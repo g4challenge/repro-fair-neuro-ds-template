4856ca86c6ca68f8587947f41bfd4a779d564c5d
g4challenge <lukashuber1@gmail.com>
2020-07-31 14:12:56 +0200

    made package buildable

--- R/packages.R
+++ R/packages.R
@@ -1,6 +1,8 @@
 # Packages
 
 
-pacman::p_load(remotes, drake)
+pacman::p_load(remotes,
+               drake,
+               tidyverse)
 remotes::install_github("https://github.com/mstr3336/mandrake/")
 remotes::install_github("mikldk/roxytest")
