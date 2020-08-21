78c039a1078f354580762add1d53a1e18a93a433
g4challenge <lukashuber1@gmail.com>
2020-08-21 15:09:28 +0200

    added requirements testing concept

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,23 @@ library(testthat)
 library(reproducibleFairTemplate)
 
 test_check("reproducibleFairTemplate")
+
+test_that("0 - context is defined", 1)
+  # strobe, record, daqcord, cdisc share
+test_that("1 - process steps exist", 1)
+  # KDD
+test_that("2 - documentation is created (roxygen)",1)
+  # roxygen, cchic, provenance, pkgdown
+test_that("3 - data are versioned and controlled", 1)
+  # FAIR, DAQCORd, IQM3, FHIR, privacy+security
+  # DVC
+test_that("4 - analysis-code is versioned and assured")
+  # environment - renv
+  # traceable  - drake
+  # pipelines - drake
+  # dockerized
+  # wholetale, dcat, nanopublications, codeocean, binder...
+test_that("5 - execution is validated", 1)
+  # drake
+test_that("6 - results are traceable", 1)
+  # zenodo,...
