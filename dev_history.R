340879a50dbecc8e79fb471d8ce7f65364b640f5
g4challenge <lukashuber1@gmail.com>
2021-02-02 13:17:23 +0100

    added link and compendium draft, yet binder does not need holepunch package

--- dev_history.R
+++ dev_history.R
@@ -8,8 +8,14 @@
 
 ## Research Compendium
 ## https://www.researchobject.org/ro-crate/1.1/root-data-entity.html#ro-crate-metadata-file-descriptor
+write_compendium_description(package = "Your compendium name",
+                             description = "Your compendium description")
+
 
 # Code
 # Lintr
 install.packages("lintr")
 library(lintr)
+#holepunch::write_runtime() # https://github.com/alan-turing-institute/the-turing-way/blob/master/workshops/boost-research-reproducibility-binder/workshop-presentations/zero-to-binder-r.md
+
+
