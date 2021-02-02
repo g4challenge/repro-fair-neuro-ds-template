2ce18e06779308f5f0313e39c08c53dd258139d3
g4challenge <lukashuber1@gmail.com>
2021-02-02 13:09:36 +0100

    added holepunch package for binder runtime

---
+++ dev_history.R
@@ -0,0 +1,15 @@
+# created by Lukas Huber
+
+# Dev-History
+
+# Provenance in R
+# https://github.com/End-to-end-provenance/RDataTracker
+
+
+## Research Compendium
+## https://www.researchobject.org/ro-crate/1.1/root-data-entity.html#ro-crate-metadata-file-descriptor
+
+# Code
+# Lintr
+install.packages("lintr")
+library(lintr)
