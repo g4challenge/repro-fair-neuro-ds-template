40d2e2b30dce3e16dd830a1c59c0f4543e167ff3
g4challenge <lukashuber1@gmail.com>
2021-02-02 11:03:50 +0100

    aiming at roxytest usethis fix

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,22 +3,25 @@ library(reproducibleFairTemplate)
 
 test_check("reproducibleFairTemplate")
 
+
 test_that("0 - context is defined", 1)
-  # strobe, record, daqcord, cdisc share
+# strobe, record, daqcord, cdisc share
 test_that("1 - process steps exist", 1)
-  # KDD
+# KDD
 test_that("2 - documentation is created (roxygen)",1)
-  # roxygen, cchic, provenance, pkgdown
+# roxygen, cchic, provenance, pkgdown
 test_that("3 - data are versioned and controlled", 1)
-  # FAIR, DAQCORd, IQM3, FHIR, privacy+security
-  # DVC
+# FAIR, DAQCORd, IQM3, FHIR, privacy+security
+# DVC
 test_that("4 - analysis-code is versioned and assured", 1)
-  # environment - renv
-  # traceable  - drake
-  # pipelines - drake
-  # dockerized
-  # wholetale, dcat, nanopublications, codeocean, binder...
+# environment - renv
+# traceable  - drake
+# pipelines - drake
+# dockerized
+# wholetale, dcat, nanopublications, codeocean, binder...
 test_that("5 - execution is validated", 1)
-  # drake
+# drake
 test_that("6 - results are traceable", 1)
-  # zenodo,...
+# zenodo,...
+
+test_that("Environment is correct version")
