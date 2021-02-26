2649e23308dad33ef7aa6bb9c71eb8772550f2b9
g4challenge <lukashuber1@gmail.com>
2021-02-26 09:04:22 +0100

    updated Tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -21,16 +21,31 @@ test_that("3 - data are versioned and controlled", 1)
 # DVC
 test_that("4 - analysis-code is versioned and assured", 1)
 # environment - renv
+test_that("4 - renv exists",{
+  expect_true(file.exists("../renv.lock"))
+})
 # traceable  - drake
 # pipelines - drake
 
 # wholetale, dcat, nanopublications, codeocean, binder...
 test_that("5 - execution is validated", 1)
+test_that("5 - dockerfile exists",{
+  expect_true(file.exists("../Dockerfile"))
+})
 # dockerized - rocker
 # drake
+test_that("5 - drake folder exists",{
+  expect_true(dir.exists("../.drake"))
+})
+
 
 test_that("6 - results are traceable", 1)
 # zenodo,...
 # Rocrate
+test_that("6 - ROCrate exists",{
+  expect_true(file.exists("../ro-crate-metadata.json"))
+})
 
-test_that("Environment is correct version")
+test_that("Environment is correct version", {
+  expect_true(T)
+})
